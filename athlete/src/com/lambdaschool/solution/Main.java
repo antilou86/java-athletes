771cc4d3ab0a341545aace175065e35af5e21f8d
antilou86 <antilou86@gmail.com>
2019-09-05 16:29:18 -0400

    MVP complete, stretch complete

--- athlete/src/com/lambdaschool/solution/Main.java
+++ athlete/src/com/lambdaschool/solution/Main.java
@@ -11,22 +11,32 @@ public class Main
         injector = new TrackAthleteCreationInjector();
         app = injector.getProcess();
         app.displayAthlete();
+            //added for stretch
+        System.out.println();
+        app.displayCourt();
 
         // Create a Hockey Athlete
         injector = new HockeyAthleteCreationInjector();
         app = injector.getProcess();
         app.displayAthlete();
-
+            //added for stretch
+        System.out.println();
+        app.displayCourt();
 
         // Create a Rugby Athlete
         injector = new RugbyAthleteCreationInjector();
         app = injector.getProcess();
         app.displayAthlete();
+            //added for stretch
+        System.out.println();
+        app.displayCourt();
 
         // Create a Baseball Athlete
         injector = new BaseballAthleteCreationInjector();
         app = injector.getProcess();
         app.displayAthlete();
-
+            //added for stretch
+        System.out.println();
+        app.displayCourt();
     }
 }
